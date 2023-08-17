9f12807f455aad8e1b20119542d1bec535c1cef3
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-08-17 19:08:35 +0000

    styles fix

--- 3dDescriptorLearning/cpp_geometry_images_generation/include/Heap.h
+++ 3dDescriptorLearning/cpp_geometry_images_generation/include/Heap.h
@@ -1,3 +1,8 @@
 /************************************************************
  * This file is part of the DGPC library. The library computes
- * Discrete Geodesic Polar Coordinates on a polygo
+ * Discrete Geodesic Polar Coordinates on a polygonal mesh.
+ *
+ * More info:
+ *   http://folk.uio.no/eivindlm/dgpc/
+ *
+ * Authors: Eivind 
