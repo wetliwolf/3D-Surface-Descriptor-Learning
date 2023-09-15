35accf68a532edd3cf32d2a2b7538767c5fb2538
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-09-15 18:04:35 +0000

    Correct typo in README.md

--- 3dDescriptorLearning/matlab_tools/compute_curvature.m
+++ 3dDescriptorLearning/matlab_tools/compute_curvature.m
@@ -1,2 +1,3 @@
 % function [Umin,Umax,Cmin,Cmax,Cmean,Cgauss,Normal] = compute_curvature(V,F)
-function [Umax, Umin, Cmax, Cmin, Nor
+function [Umax, Umin, Cmax, Cmin, Normal, hks, diameter] = compute_curvature(V, F, off_filename, hks_len)
+% c
