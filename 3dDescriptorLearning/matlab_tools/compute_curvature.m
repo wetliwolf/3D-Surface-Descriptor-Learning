44966f5aef46eea1ae8d14c18fb04cf146bd3ac0
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-09-15 18:20:53 +0000

    add paginator

--- 3dDescriptorLearning/matlab_tools/compute_curvature.m
+++ 3dDescriptorLearning/matlab_tools/compute_curvature.m
@@ -1,3 +1,5 @@
 % function [Umin,Umax,Cmin,Cmax,Cmean,Cgauss,Normal] = compute_curvature(V,F)
 function [Umax, Umin, Cmax, Cmin, Normal, hks, diameter] = compute_curvature(V, F, off_filename, hks_len)
-% c
+% compute_curvature - compute principal curvature directions and values
+%
+%   [Umin,Uma
