670e35554b80776be3da06c717afaa9a4a75b343
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-09-15 22:42:06 +0000

    Fix comma

--- 3dDescriptorLearning/matlab_tools/compute_curvature.m
+++ 3dDescriptorLearning/matlab_tools/compute_curvature.m
@@ -2,4 +2,6 @@
 function [Umax, Umin, Cmax, Cmin, Normal, hks, diameter] = compute_curvature(V, F, off_filename, hks_len)
 % compute_curvature - compute principal curvature directions and values
 %
-%   [Umin,Uma
+%   [Umin,Umax,Cmin,Cmax,Cmean,Cgauss,Normal] = compute_curvature(V,F,options);
+%
+%   Umin is the di
