6ac183939982fde7e56105eb9de7c912d3071e74
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-11-12 19:29:17 +0000

    Whitespace

--- 3dDescriptorLearning/matlab_tools/compute_curvature.m
+++ 3dDescriptorLearning/matlab_tools/compute_curvature.m
@@ -170,4 +170,6 @@ hks = OFF2HKS(off_filename, hks_len)';
 function z = crossp(x,y)
 % x and y are (m,3) dimensional
 z = x;
-z(:,1) = x(:,2).*y(:,3) - x(:,3).*y(:
+z(:,1) = x(:,2).*y(:,3) - x(:,3).*y(:,2);
+z(:,2) = x(:,3).*y(:,1) - x(:,1).*y(:,3);
+z(:,3) = x(:,1).*y(:,2) - x(:
