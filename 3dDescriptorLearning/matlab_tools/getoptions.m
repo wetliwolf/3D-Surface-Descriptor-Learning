2259ba567a0c08ad71ca29e8e1e64c0a798ba13a
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-11-14 12:38:35 +0000

    interpolation

--- 3dDescriptorLearning/matlab_tools/getoptions.m
+++ 3dDescriptorLearning/matlab_tools/getoptions.m
@@ -1,1 +1,5 @@
-function v = getoptions(options, name, v, mendator
+function v = getoptions(options, name, v, mendatory)
+
+% getoptions - retrieve options parameter
+%
+%   v = getoptions(options, 'entry', v0);
