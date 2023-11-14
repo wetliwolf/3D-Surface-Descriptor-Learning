d2b75364d6334deabb618053675f3d1e5f0c4ac2
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-11-14 17:07:03 +0000

    prova

--- 3dDescriptorLearning/matlab_tools/getoptions.m
+++ 3dDescriptorLearning/matlab_tools/getoptions.m
@@ -3,3 +3,6 @@ function v = getoptions(options, name, v, mendatory)
 % getoptions - retrieve options parameter
 %
 %   v = getoptions(options, 'entry', v0);
+% is equivalent to the code:
+%   if isfield(options, 'entry')
+%       v = options
