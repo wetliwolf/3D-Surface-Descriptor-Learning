58359e2d3daa3c06cb41ee63c3613ea16d599fea
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-11-26 08:49:55 +0000

    new plot

--- 3dDescriptorLearning/matlab_tools/perform_mesh_smoothing.m
+++ 3dDescriptorLearning/matlab_tools/perform_mesh_smoothing.m
@@ -2,4 +2,6 @@ function f = perform_mesh_smoothing(face,vertex,f,options)
 
 % perform_mesh_smoothing - smooth a function defined on a mesh by averaging
 %
-%   f
+%   f = perform_mesh_smoothing(face,vertex,f,options);
+%
+%   Smooth a function f
