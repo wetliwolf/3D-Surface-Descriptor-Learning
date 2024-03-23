4a66c140ba47c76b623a6ef8f0d25cf33f58c243
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-03-23 12:36:06 +0000

    Bump version: 0.3.1 → 0.4.0

--- 3dDescriptorLearning/python_tripletCNN/train_softmax256.py
+++ 3dDescriptorLearning/python_tripletCNN/train_softmax256.py
@@ -162,3 +162,82 @@ class TripletNet:
 
             """ conv2 """
             network = Conv2d(network, n_filter=128, filter_size=(3, 3), strides=(1, 1), act=tf.identity,
+                             padding='SAME', W_init=args.conv_initializer, name='conv2_1')
+
+            network = BatchNormLayer(network, decay=0.9, epsilon=1e-4, act=args.activation,
+                                     is_train=self.is_training, name='bn2_1')
+
+            network = MaxPool2d(network, filter_size=(2, 2), strides=(2, 2),
+                                padding='SAME', name='pool2')
+
+            """ conv3 """
+            network = Conv2d(network, n_filter=256, filter_size=(3, 3), strides=(1, 1), act=tf.identity,
+                             padding='SAME', W_init=args.conv_initializer, name='conv3_1')
+
+            network = BatchNormLayer(network, decay=0.9, epsilon=1e-4, act=args.activation,
+                                     is_train=self.is_training, name='bn3_1')
+
+            network = MaxPool2d(network, filter_size=(2, 2), strides=(2, 2),
+                                padding='SAME', name='pool3')
+
+            """ conv4 """
+            network = Conv2d(network, n_filter=512, filter_size=(3, 3), strides=(1, 1), act=tf.identity,
+                             padding='SAME', W_init=args.conv_initializer, name='conv4_1')
+
+            network = BatchNormLayer(network, decay=0.9, epsilon=1e-4, act=args.activation,
+                                     is_train=self.is_training, name='bn4_1')
+
+            network = MeanPool2d(network, filter_size=(2, 2), strides=(2, 2),
+                                 padding='SAME', name='pool4')
+
+
+            network = FlattenLayer(network, name='flatten')
+            network = DenseLayer(network, n_units=512, act=tf.identity, name='fc1_relu')
+
+            network = BatchNormLayer(network, decay=0.9, epsilon=1e-4, act=args.activation,
+                                     is_train=self.is_training, name='bn_fc')
+            # network = DenseLayer(network, n_units=4096, act=args.activation, name='fc2_relu')
+            # network = DenseLayer(network, n_units=10, act=tf.identity, name='fc3_relu')
+            network = DenseLayer(network, n_units=128, act=tf.identity, name='128d_embedding')
+
+        return network
+
+    # Method to construct the Triplet CNNs (3 parameter-shared CNN) using inference
+    def build_nets(self, anchor_placeholder, positive_placeholder, negative_placeholder, anchor_label_placeholder, keypoint_num):
+        self.anchor_net = self.inference(anchor_placeholder, reuse=None)
+        self.positive_net = self.inference(positive_placeholder, reuse=True)
+        self.negative_net = self.inference(negative_placeholder, reuse=True)
+
+        gap = tf.constant(np.float32(args.triplet_loss_gap))
+        zero = tf.constant(np.float32(0))
+
+        self.all_multiuse_params = self.anchor_net.all_params.copy()
+
+        self.anchor_net.outputs = args.activation(self.anchor_net.outputs)
+
+        self.anchor_net = DenseLayer(self.anchor_net, n_units=keypoint_num, act=tf.identity, name='feature')
+
+        logits = self.anchor_net.outputs
+        self.predictions = tf.nn.softmax(logits)
+        self.cost = tl.cost.cross_entropy(output=logits, target=anchor_label_placeholder, name='cost')
+
+        correct_prediction = tf.equal(tf.cast(tf.argmax(logits, 1), tf.int32), anchor_label_placeholder)
+        self.acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='acc')
+
+
+        # self.cost_same = tf.norm(self.anchor_net.outputs - self.positive_net.outputs)
+        # self.cost_diff = tf.norm(self.anchor_net.outputs - self.negative_net.outputs)
+        #
+        # delta = self.cost_same - self.cost_diff
+        #
+        # # self.cost = tf.maximum(zero, gap + delta)
+        # ratio = self.cost_diff / self.cost_same
+        # self.cost = - (gap + self.cost_diff) / (gap + self.cost_same) + self.cost_same
+        
+        # Add them to tf.summary to see them in tensorboard
+        tf.summary.scalar(name='cost', tensor=self.cost)
+        # tf.summary.scalar(name='delta', tensor=delta)
+        # tf.summary.scalar(name='ratio', tensor=ratio)
+        # tf.summary.scalar(name='cost_same', tensor=self.cost_same)
+        # tf.summary.scalar(name='cost_diff', tensor=self.cost_diff)
+        tf.summary.scalar(name='accuracy', tensor=self.acc)
